0963c097b54e07bf908d8c0b45a53b3d2d158573
TechPrototyper <110817746+TechPrototyper@users.noreply.github.com>
2024-04-25 16:49:10 +0200

    Update Git.cs - Use Commit Id as last deployed Version
    Normally, a commit Id uses makes a diff between commit id and commit id^. But here, we want to make a diff between HEAD and commit id, so that we can rollup any changes, while providing the a version that we know to be in synch with the APIM Service.

--- tools/code/publisher/Git.cs
+++ tools/code/publisher/Git.cs
@@ -49,7 +49,7 @@ internal static class Git
 
     private static async ValueTask<string> GetDiffTreeOutput(CommitId commitId, DirectoryInfo baseDirectory)
     {
-        var command = Command.Run("git", "-C", baseDirectory.FullName, "diff-tree", "--no-commit-id", "--name-status", "--relative", "-r", $"{commitId}^", $"{commitId}");
+        var command = Command.Run("git", "-C", baseDirectory.FullName, "diff-tree", "--no-commit-id", "--name-status", "--relative", "-r", $"{commitId}", "HEAD");
         var commandResult = await command.Task;
 
         return commandResult.Success
